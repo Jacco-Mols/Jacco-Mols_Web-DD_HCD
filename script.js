49dada313409ece1c404e2e0c66d4780c201346b
Jacco12 <molsjacco@gmail.com>
2026-04-05 18:37:57 +0200

    shortened trnascript - styling transcript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // transcribe of text source: https://spotscribe.io/podcasts/episode/51613591711
 // example of transcrips in js using vtt files - https://james.cridland.net/blog/2025/html-audio-player-with-captions/
 // const audio = document.getElementById('podcast-audio');
@@ -16,57 +15,38 @@
 //     switchSpeaker()
 // });
 
-
-// function switchSpeaker(text) {
-
-//     const people = document.querySelectorAll('.speaker')
-//     const person1 = document.querySelector('')
-//     const person2 = document.querySelector('')
-//     const person3 = document.querySelector('')
-
-//     people.forEach(p => 
-//         p.classList.remove('active')
-//     )
-
-//     if (text.includes('SPEAKER 1')) {
-//         person1.classList.add('.active')
-//     }
-
-//     if (text.includes('SPEAKER 2')) {
-//        person2.classList.add('.active')
-//     }
-    
-
-//     if (text.includes('SPEAKER 3')) {
-//        person3.classList.add('.active')
-//     }
-// }
-
-
-
-
-
 const audio = document.getElementById('podcast-audio');
 const transcript = document.getElementById('podcast-transcript');
 
 audio.addEventListener('loadedmetadata', () => {
     const track = audio.textTracks[0];
-    track.mode = 'hidden';
     
+    track.mode = 'hidden';
+
     track.addEventListener('cuechange', () => {
 
         if (track.activeCues.length > 0) {
             const cue = track.activeCues[0];
-            const text = cue.text
+            const text = cue.text;
 
-            transcript.innerText = text
+            transcript.innerHTML = text;
 
-            switchSpeaker(text)
+            if(text.includes('.music-playing')){
+                transcript.style.setProperty('color', '#fff');
+            }
+            else {
+                transcript.style.setProperty('color', '#000' )
+            }
+            if(text.includes('.scream')) {
+                transcript.style.setProperty('font-size', '10em')
+            }
+            else {
+                transcript.style.setProperty('font-size', '2em')
+            }
+
+            switchSpeaker(text);
         }
-
-
-    });
-
+    })
 })
 
 function switchSpeaker(text) {
